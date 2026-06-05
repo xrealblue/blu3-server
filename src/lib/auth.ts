288ef32c618662b00082719f13b7873802bf651b
xRealBlue <heetvavadiya456@gmail.com>
2026-06-06 01:11:32 +0530

    @auth update

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,6 +5,7 @@ import { db } from "../db/index.js";
 import * as schema from "../db/schema.js";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL!,
   database: drizzleAdapter(db, {
     provider: "pg",
     schema: {
